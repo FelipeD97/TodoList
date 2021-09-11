72afa7c41f198be91be712dc540ad1c1055798e8
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2021-09-11 13:53:31 -0400

    Added, filter function

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 
 //EVENT LISTENERS
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('change', filterTodo);
 
 
 //FUNCTIONS
@@ -41,14 +43,43 @@ function addTodo(event) {
 function deleteCheck(e) {
     const item = e.target;
     //DELETE TODO
-    console.log(e.target);
     if(item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
-        todo.remove();
+        //ADD ANIMATION
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        })
     }
 
     if(item.classList[0] === 'complete-btn'){
         const todo = item.parentElement;
         todo.classList.toggle("completed");
+        console.log(todo);
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                }else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                }else {
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    })
 }
